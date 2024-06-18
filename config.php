cc923004c1ef8730dbad45081b843f86d9c78450
Daniel <dbermu89@gmail.com>
2024-06-18 09:08:55 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $XVWA_WEBROOT = "";
 $host = "localhost";
 $dbname = 'xvwa';
 $user = "root";
-$pass = "a_F3k3_Password";
+$password = "948493284JDKASJDKASjdksjdks434&$#";
 $conn = new mysqli($host,$user,$pass,$dbname);
 $conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
